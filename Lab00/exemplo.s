85f6cd9bb4f4b17670ec9826b83736ff93596c83
suzanapachelle <suzi.pachelle@gmail.com>
2024-03-23 15:59:22 -0300

    Sequencia de fibonacci está sendo salva na memória

--- Lab00/exemplo.s
+++ Lab00/exemplo.s
@@ -4,55 +4,85 @@
 ; 12/03/2018
 
 ; -------------------------------------------------------------------------------
-        THUMB                        ; Instruções do tipo Thumb-2
+        THUMB                        ; Instrucoes do tipo Thumb-2
 ; -------------------------------------------------------------------------------
-; Declarações EQU - Defines
+; Declaracoes EQU - Defines
 ;<NOME>         EQU <VALOR>
 ALEATORIOS EQU 0x20000400
 ORDENADOS EQU 0x20000500
 TAMANHO EQU 23
 ; -------------------------------------------------------------------------------
-; Área de Dados - Declarações de variáveis
+; Area de Dados - Declaracoes de variaveis
 		AREA  DATA, ALIGN=2
 		
-		; Se alguma variável for chamada em outro arquivo
-		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
+		; Se alguma vari?vel for chamada em outro arquivo
+		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variavel <var> a 
 		                                   ; partir de outro arquivo
-;<var>	SPACE <tam>                        ; Declara uma variável de nome <var>
+;<var>	SPACE <tam>                        ; Declara uma vari?vel de nome <var>
                                            ; de <tam> bytes a partir da primeira 
-                                           ; posição da RAM		
+                                           ; posi??o da RAM		
 ; -------------------------------------------------------------------------------
-; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
-;                  código
+; Area de Codigo - Tudo abaixo da diretiva a seguir sera armazenado na memoria de 
+;                  codigo
         AREA    |.text|, CODE, READONLY, ALIGN=2
 
-		; Se alguma função do arquivo for chamada em outro arquivo	
-        EXPORT Start                ; Permite chamar a função Start a partir de 
+		; Se alguma funcao do arquivo for chamada em outro arquivo	
+        EXPORT Start                ; Permite chamar a funcao Start a partir de 
 			                        ; outro arquivo. No caso startup.s
 									
-		; Se chamar alguma função externa	
+		; Se chamar alguma funcao externa	
         ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
-									; função <func>
+									; funcao <func>
 
 ; -------------------------------------------------------------------------------
-; Função main()
+; Funcao main()
 Start  
-; Comece o código aqui <======================================================
-	LDR R0, =valores 			; carrega em r0 o endereço inicial do vetor valores
-	LDR R1, =ALEATORIOS 		; carrega em r1 o endereço inicial da RAM para salvar
+; Comece o codigo aqui <======================================================
+	LDR R0, =valores 			; carrega em r0 o endereco inicial do vetor valores
+	LDR R1, =ALEATORIOS 		; carrega em r1 o endereco inicial da RAM para salvar
 	LDR R2, =TAMANHO			; carrega em r2 o tamanho do vetor
-	MOV R9, #0					; inicia um contador
+	MOV R3, #0					; inicia um contador
 
-loop
-	LDRB R3, [R0], #1		; r3 recebe o valor guardado no endereço de r0
-	STRB R3, [R1], #1		; endereço guardado em r1 recebe o valor de r3
-	ADD R9, R9, #1			; contador ++
-	CMP R2, R9				; verifica se o contador atingiu o tamanho do vetor
-	BNE loop
+carregaAleatorios
+	LDRB R4, [R0], #1		; r4 recebe o valor guardado no endereco de r0
+	STRB R4, [R1], #1		; endereco guardado em r1 recebe o valor de r4
+	ADD R3, R3, #1			; contador ++
+	CMP R2, R3				; verifica se o contador atingiu o tamanho do vetor
+	BNE carregaAleatorios
+	
+	LDR R5, =ORDENADOS			; carrega em r5 o endereco inicial do RAM para salvar os numeros a serem ordenados
+	LDR R1, =ALEATORIOS 		; carrega em r1 o endereco inicial da RAM para ler os valores da lista
+	LDR R2, =TAMANHO			; carrega em r2 o tamanho do vetor
+	MOV R6, #0					; inicializa um contador para armazenar o tamanho da lista a ser ordenada
+	MOV R3, #0					; inicia um contador da lista de aleatorios
+	LDRB R4, [R1], #1			; r4 recebe o valor guardado no endereco de r1
+	
+reiniciaFibonacci
+	MOV R7, #0					; a(n-2) da sequencia de fibonacci
+	MOV R8, #1					; a(n-1) da sequencia de fibonacci
+	
+fibonacci
+	ADD R9, R7, R8				; carrega em r9 (que é o an) o valor de r7 + r8
+	MOV R7, R8					; carrega em r7 o valor de r8 (sera o novo a(n-2))
+	MOV R8, R9					; carrega em r8 o valor de r9 (sera o novo a(n-1))
+	CMP R4, R9					
+	BLO proximoLista
+	BNE fibonacci
+	ADD R6, R6, #1				; incremeta contador
+	STRB R4, [R5], #1			; endereco guardado em r5 recebe o valor de r4 que esta na sequencia de fibonacci
+	
+proximoLista
+	ADD R3, R3, #1				; contador ++
+	CMP R2, R3					; verifica se o contador atingiu o tamanho do vetor
+	IT NE
+		LDRBNE R4, [R1], #1		; r4 recebe o valor guardado no endereco de r1 se o numero da lista do menos que an
+	BNE reiniciaFibonacci 
+	
 
 
 	
 valores DCB 3, 244, 14, 233, 1, 6, 9, 18, 13, 254, 21, 34, 2, 67, 135,  8, 89, 43, 5, 105, 144, 201, 55
-
-    ALIGN                           ; garante que o fim da seção está alinhada 
+	NOP
+	
+    ALIGN                           ; garante que o fim da se??o est? alinhada 
     END                             ; fim do arquivo
