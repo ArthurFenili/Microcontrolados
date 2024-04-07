9eff3911d732f5fa327dfaf0241243cd3db56d5a
Arthur <arthurfenilidasilva@gmail.com>
2024-04-07 15:21:15 -0300

    leds piscando em sequencia

--- Lab01/gpio.s
+++ Lab01/gpio.s
@@ -65,6 +65,19 @@ GPIO_PORTB_AHB_PUR_R     	EQU    0x40059510
 GPIO_PORTB_AHB_DATA_R    	EQU    0x400593FC
 GPIO_PORTB_AHB_DATA_BITS_R  EQU    0x40059000
 GPIO_PORTB               	EQU    2_000000000000010 ; SYSCTL_PPGPIO_P1
+	
+; PORT P
+GPIO_PORTP_LOCK_R       	EQU    0x40065520
+GPIO_PORTP_CR_R        	    EQU    0x40065524
+GPIO_PORTP_AMSEL_R      	EQU    0x40065528
+GPIO_PORTP_PCTL_R       	EQU    0x4006552C
+GPIO_PORTP_DIR_R        	EQU    0x40065400
+GPIO_PORTP_AFSEL_R      	EQU    0x40065420
+GPIO_PORTP_DEN_R        	EQU    0x4006551C
+GPIO_PORTP_PUR_R        	EQU    0x40065510
+GPIO_PORTP_DATA_R       	EQU    0x400653FC
+GPIO_PORTP_DATA_BITS_R  	EQU    0x40065000
+GPIO_PORTP					EQU	   2_010000000000000
 
 ; -------------------------------------------------------------------------------
 ; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
@@ -77,8 +90,15 @@ GPIO_PORTB               	EQU    2_000000000000010 ; SYSCTL_PPGPIO_P1
 		EXPORT PortB_Output			; Permite chamar PortB_Output de outro arquivo
 		EXPORT PortA_Output          ; Permite chamar PortJ_Input de outro arquivo
 		EXPORT PortJ_Input          ; Permite chamar PortJ_Input de outro arquivo
+		EXPORT PortP_Output
+		EXPORT LED_Output
+		EXPORT Display_Output
+			
+			
+		IMPORT Pisca_Transistor_PP5
 		
-									
+; Mapeamento dos 7 segmentos (0 a F)
+MAPEAMENTO_7SEG DCB	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F								
 
 ;--------------------------------------------------------------------------------
 ; Função GPIO_Init
@@ -95,6 +115,7 @@ GPIO_Init
 			ORR     R1, #GPIO_PORTJ					;Seta o bit da porta J, fazendo com OR
 			ORR     R1, #GPIO_PORTB					;Seta o bit da porta B, fazendo com OR
 			ORR     R1, #GPIO_PORTA					;Seta o bit da porta A, fazendo com OR
+			ORR     R1, #GPIO_PORTP					;Seta o bit da porta P, fazendo com OR
             STR     R1, [R0]						;Move para a memória os bits das portas no endereço do RCGCGPIO
  
             LDR     R0, =SYSCTL_PRGPIO_R			;Carrega o endereço do PRGPIO para esperar os GPIO ficarem prontos
@@ -103,6 +124,7 @@ EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do regis
 			ORR     R2, #GPIO_PORTJ                 ;Seta o bit da porta J, fazendo com OR
 			ORR     R2, #GPIO_PORTB                 ;Seta o bit da porta B, fazendo com OR
 			ORR     R2, #GPIO_PORTA                 ;Seta o bit da porta A, fazendo com OR
+			ORR     R2, #GPIO_PORTP					;Seta o bit da porta P, fazendo com OR
             TST     R1, R2							;ANDS de R1 com R2
             BEQ     EsperaGPIO					    ;Se o flag Z=1, volta para o laço. Senão continua executando
  
@@ -119,6 +141,9 @@ EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do regis
  
 			LDR     R0, =GPIO_PORTA_AHB_AMSEL_R		;Carrega o R0 com o endereço do AMSEL para a porta B
             STR     R1, [R0]					    ;Guarda no registrador AMSEL da porta B da memória
+			
+			LDR		R0, =GPIO_PORTP_AMSEL_R			;Carrega o R0 com o endereço do AMSEL para a porta P
+			STR 	R1, [R0]						;Guarda no registrador AMSEL da porta P da memória
 ; 3. Limpar PCTL para selecionar o GPIO
             MOV     R1, #0x00					    ;Colocar 0 no registrador para selecionar o modo GPIO
             LDR     R0, =GPIO_PORTJ_AHB_PCTL_R		;Carrega o R0 com o endereço do PCTL para a porta J
@@ -132,6 +157,9 @@ EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do regis
 			
 			LDR     R0, =GPIO_PORTA_AHB_PCTL_R      ;Carrega o R0 com o endereço do PCTL para a porta A
             STR     R1, [R0]                        ;Guarda no registrador PCTL da porta A da memória
+			
+			LDR		R0, =GPIO_PORTP_PCTL_R			;Carrega o R0 com o endereço do PCTL para a porta P
+			STR		R1, [R0]						;Guarda no registrador PCTL da porta P da memória
 ; 4. DIR para 0 se for entrada, 1 se for saída
             LDR     R0, =GPIO_PORTQ_DIR_R			; Carrega o R0 com o endereço do DIR para a porta Q
 			MOV     R1, #2_00001111					; PQ0:PQ3
@@ -142,12 +170,16 @@ EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do regis
             STR     R1, [R0]						;Guarda no registrador PCTL da porta J da memória
 			
 			LDR     R0, =GPIO_PORTB_AHB_DIR_R		;Carrega o R0 com o endereço do DIR para a porta B
-            MOV     R1, #2_00110000               	;PB4 e PB3
+            MOV     R1, #2_00110000               	;PB4 e PB5
             STR     R1, [R0]						;Guarda no registrador
 			
 			LDR     R0, =GPIO_PORTA_AHB_DIR_R		;Carrega o R0 com o endereço do DIR para a porta A
-            MOV     R1, #2_11110000               	;PA0:PA3
+            MOV     R1, #2_11110000               	;PA4:PA7
             STR     R1, [R0]						;Guarda no registrador
+			
+			LDR		R0, =GPIO_PORTP_DIR_R			;Carrega o R0 com o endereço do DIR para a porta P
+			MOV 	R1, #2_00100000					;PP5
+			STR		R1, [R0]						;Guarda no registrador
 ; 5. Limpar os bits AFSEL para 0 para selecionar GPIO 
 ;    Sem função alternativa
             MOV     R1, #0x00						;Colocar o valor 0 para não setar função alternativa
@@ -162,6 +194,9 @@ EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do regis
 			
 			LDR     R0, =GPIO_PORTA_AHB_AFSEL_R     ;Carrega o endereço do AFSEL da porta A
             STR     R1, [R0]                        ;Escreve na porta
+			
+			LDR 	R0, =GPIO_PORTP_AFSEL_R			;Carrega o endereço do AFSEL na porta P
+			STR		R1, [R0]						;Escreve na porta
 ; 6. Setar os bits de DEN para habilitar I/O digital
             LDR     R0, =GPIO_PORTQ_DEN_R				;Carrega o endereço do DEN
             MOV     R1, #2_00001111                     ;Ativa os pinos PQ0:PQ3 como I/O Digital
@@ -172,13 +207,16 @@ EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do regis
             STR     R1, [R0]                            ;Escreve no registrador da memória funcionalidade digital
 			
 			LDR     R0, =GPIO_PORTB_AHB_DEN_R			;Carrega o endereço do DEN
-            MOV     R1, #2_00110000                     ;Setar os pinos PB3 e PB4
+            MOV     R1, #2_00110000                     ;Setar os pinos PB4 e PB5
             STR     R1, [R0]							;Escreve no registrador da memória funcionalidade digital 
 			
 			LDR     R0, =GPIO_PORTA_AHB_DEN_R			;Carrega o endereço do DEN
             MOV     R1, #2_11110000                     ;Setar os pinos PA0:PA3
             STR     R1, [R0]							;Escreve no registrador da memória funcionalidade digital 
 			
+			LDR		R0, =GPIO_PORTP_DEN_R				;Carrega o endereço do DEN
+			MOV 	R1, #2_00100000						;Setar o pino PP5
+			STR		R1, [R0]							;Escreve no registrador de memória funcionalidade digital
 ; 7. Para habilitar resistor de pull-up interno, setar PUR para 1
 			LDR     R0, =GPIO_PORTJ_AHB_PUR_R			;Carrega o endereço do PUR para a porta J
 			MOV     R1, #2_00000011						;Habilitar funcionalidade digital de resistor de pull-up 
@@ -240,6 +278,52 @@ PortJ_Input
 	LDR R0, [R1]                            ;Lê no barramento de dados dos pinos [J1-J0]
 	BX LR									;Retorno
 
+PortP_Output
+	LDR	R1, =GPIO_PORTP_DATA_R		    ;Carrega o valor do offset do data register
+	;Read-Modify-Write para escrita
+	LDR R2, [R1]
+	BIC R2, #2_00100000                     ;Limpar bits lidos da porta 11001111
+	ORR R0, R0, R2                          ;Fazer o OR do lido pela porta com o parâmetro de entrada
+	STR R0, [R1]                            ;Escreve na porta B o barramento de dados dos pinos N1
+	BX LR	
+
+LED_Output
+	PUSH {LR}
+	LDR	R1, =GPIO_PORTA_AHB_DATA_R		    ;Carrega o valor do offset do data register
+	LDR	R2, =GPIO_PORTQ_DATA_R		    	;Carrega o valor do offset do data register
+                        
+	LDR R3, [R1]
+	LDR R4, [R2]
+
+
+	CMP R9, #0
+	ITT EQ
+		MOVEQ R3, #2_10000000
+		MOVEQ R4, #2_00000001
+		
+	CMP R9, #1
+	ITT EQ
+		MOVEQ R3, #2_01000000
+		MOVEQ R4, #2_00000010
+		
+	CMP R9, #2
+	ITT EQ
+		MOVEQ R3, #2_00100000
+		MOVEQ R4, #2_00000100
+		
+	CMP R9, #3
+	ITT EQ
+		MOVEQ R3, #2_00010000
+		MOVEQ R4, #2_00001000
+	
+	STR R3, [R1]
+	STR R4, [R2]
+	
+	BL Pisca_Transistor_PP5
+	POP {LR}
+	BX LR		
+	
+Display_Output
 
 
 
--- Lab01/main.s
+++ Lab01/main.s
@@ -34,6 +34,9 @@
 		; Se alguma função do arquivo for chamada em outro arquivo	
         EXPORT Start                ; Permite chamar a função Start a partir de 
 			                        ; outro arquivo. No caso startup.s
+		EXPORT Pisca_Transistor_PP5
+
+
 									
 		; Se chamar alguma função externa	
         ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
@@ -46,6 +49,9 @@
 		IMPORT  PortB_Output
 		IMPORT  PortA_Output
         IMPORT  PortJ_Input	
+		IMPORT 	PortP_Output
+		IMPORT LED_Output
+		IMPORT Display_Output
 
 ; Mapeamento dos 7 segmentos (0 a F)
 MAPEAMENTO_7SEG DCB	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F
@@ -57,6 +63,8 @@ Start
 	BL GPIO_Init                 ;Chama a subrotina que inicializa os GPIO
 	
 	LDR R11, =MAPEAMENTO_7SEG	 ; Desloca escolhendo o respectivo número das unidades
+	MOV R9, #0					 ; Contador para os leds
+
 
 MainLoop
 ;Verifica_Nenhuma
@@ -69,62 +77,79 @@ MainLoop
 ;Verifica_SW1	
 ;	CMP R0, #2_00000000			 ;Verifica se somente a chave SW1 está pressionada
 
-	MOV R1, #2					; contador proximo numero
-	
-	LDRB R10, [R11, R1]
-	AND R0, R10, #2_11110000	; Atualiza DSDP:DSE (PA7:PA4)
-	BL PortA_Output
-	AND R0, R10, #2_00001111	; Atualiza DSD:DSA (PQ3:PQ0)
-	BL PortQ_Output
-	MOV R0, #2_00110000			 ; Ativa o transistor do DS1 (PB4 e PB5)
-	BL PortB_Output
+;contador a partir daqui
+;	MOV R1, #2					; contador proximo numero
+;	
+;	LDRB R10, [R11, R1]
+;	AND R0, R10, #2_11110000	; Atualiza DSDP:DSE (PA7:PA4)
+;	BL PortA_Output
+;	AND R0, R10, #2_00001111	; Atualiza DSD:DSA (PQ3:PQ0)
+;	BL PortQ_Output
+;	MOV R0, #2_00110000			 ; Ativa o transistor do DS1 (PB4 e PB5)
+;	BL PortB_Output
 
-;logica do contador
+;;logica do contador
 
 
-	MOV R0, #0; -- contador;
-	MOV R1, #3; -- passo;
-	MOV R2, #0; -- sentido (1 crescente 0 decrescente) ;
-	
-;quando a chave 2 for apertada, troca o valor de R2 pra 0 se tiver 1 e 1 se tiver zero e dai dá B pra essa função
-verificaChave2
-	
-	CMP R2, #1;
-	BEQ contadorCrescente;
-	BNE contadorDecrescente;
+;	MOV R0, #0; -- contador;
+;	MOV R1, #1; -- passo;
+;	MOV R2, #0; -- sentido (1 crescente 0 decrescente) ;
+;	
+;;quando a chave 2 for apertada, troca o valor de R2 pra 0 se tiver 1 e 1 se tiver zero e dai dá B pra essa função
+;verificaChave2
+;	
+;	CMP R2, #1;
+;	BEQ contadorCrescente;
+;	BNE contadorDecrescente;
 
-;quando a chave 1 for apertada, dá B pra essa função direto, ela ja faz a comparação
-verificaChave1
+;;quando a chave 1 for apertada, dá B pra essa função direto, ela ja faz a comparação
+;verificaChave1
 
-	CMP R1, #9;
-	IT LT
-		ADDLT R1,#1;
-		MOVHS R1,#0;
-	B verificaChave1
+;	CMP R1, #9;
+;	IT LT
+;		ADDLT R1,#1;
+;		MOVHS R1,#0;
+;	B verificaChave1
 
-;chamado na função da chave2
-contadorCrescente
+;;chamado na função da chave2
+;contadorCrescente
+;	BL Display_Output
+;	CMP R7, #99;
+;	IT LT
+;		ADDLT R7, R6;
+;		BLT contadorCrescente;
+;	
+;;chamado na função da chave2	
+;contadorDecrescente
 
-	CMP R0, #99;
-	IT LT
-		ADDLT R0, R1;
-		MOVHS R0, #0;
-		
-	B contadorCrescente;
-	
-;chamado na função da chave2	
-contadorDecrescente
+;	CMP R0, #0;
+;	IT HI
+;		SUBHI R0, R1;
+;		MOVLS R0, #99;
+;		
+;	B contadorDecrescente;
 
-	CMP R0, #0;
+; Pisca LED de fora pra dentro
+	MOV R8, #1
+	BL LED_Output
+	ADD R9, R9, R8
+	CMP R9, #3
 	IT HI
-		SUBHI R0, R1;
-		MOVLS R0, #99;
-		
-	B contadorDecrescente;
-		
-		
-	NOP;
+		MOVHI R9, #0
+	B MainLoop
 
+Pisca_Transistor_PP5
+	MOV R0, #2_00100000
+	PUSH {LR}
+	BL PortP_Output
+	MOV R0, #1000
+	BL SysTick_Wait1ms
+	MOV R0, #2_00000000
+	BL PortP_Output
+	MOV R0, #1000
+	BL SysTick_Wait1ms
+	POP {LR}
+	BX LR
 
 	
 	
